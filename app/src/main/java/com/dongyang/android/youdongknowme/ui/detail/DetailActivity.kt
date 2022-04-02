747d2cbbc597d0426c78f0d209a239c55c3edaf2
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-04-02 21:40:09 +0900

    feat : Main, Detail UI 작성
    * 기본적인 틀을 잡기 위해 대표 화면 두 가지만 우선적으로 UI 작성하였음.
    * 추후 변경 가능

---
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.dongyang.android.youdongknowme.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongyang.android.youdongknowme.R
+
+/* 공지사항 글 상세 화면 */
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/com/dongyang/android/youdongknowme/ui/main/MainActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/main/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.dongyang.android.youdongknowme.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.dongyang.android.youdongknowme.R
+import com.dongyang.android.youdongknowme.ui.detail.DetailActivity
 
 /* 메인 화면 */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testCode = findViewById<TextView>(R.id.main_title)
+
+        testCode.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
