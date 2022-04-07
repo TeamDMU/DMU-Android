bd09013cdefc6c776c119c26720236d57908f012
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-04-07 16:53:37 +0900

    feat : 하단 메뉴 활성화

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.dongyang.android.youdongknowme.ui.view.main
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.ActivityMainBinding
-import com.dongyang.android.youdongknowme.ui.view.detail.DetailActivity
 import com.dongyang.android.youdongknowme.ui.view.notice.NoticeFragment
+import com.dongyang.android.youdongknowme.ui.view.schedule.ScheduleFragment
+import com.dongyang.android.youdongknowme.ui.view.setting.SettingFragment
+
 
 /* 메인 액티비티 */
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +23,30 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, NoticeFragment.newInstance())
+            .commit()
+
+        initNavigationView()
+    }
+
+    private fun initNavigationView() {
+        binding.mainNvBottom.run {
+            setOnItemSelectedListener { item ->
+                replaceFragment(
+                    when (item.itemId) {
+                        R.id.menu_main_home -> NoticeFragment.newInstance()
+                        R.id.menu_main_setting -> ScheduleFragment.newInstance()
+                        else -> SettingFragment.newInstance()
+                    }
+                )
+                true
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, fragment)
             .commit()
     }
 }
