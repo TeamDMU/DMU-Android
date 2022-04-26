991853692452a35378fa2d3b1afc68908b070bbb
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-04-26 17:13:09 +0900

    feat : 학사 일정 기본 리사이클러뷰 구성 완료
    * 학사 일정탭 리사이클러뷰 추가 및 데이터 테스트 완료
    * API 구축 완료 후 데이터 변경 필요

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleFragment.kt
@@ -1,10 +1,13 @@
 package com.dongyang.android.youdongknowme.ui.view.schedule
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.FragmentScheduleBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseFragment
 import com.dongyang.android.youdongknowme.standard.util.log
+import com.dongyang.android.youdongknowme.ui.adapter.ScheduleAdapter
 
 
 /* 학사 일정 화면 */
@@ -18,14 +21,21 @@ class ScheduleFragment : BaseFragment<FragmentScheduleBinding, ScheduleViewModel
         ViewModelProvider(this)[ScheduleViewModel::class.java]
     }
 
+    private lateinit var adapter : ScheduleAdapter
+
     override fun initStartView() {
         log(binding.scheduleCalendar.currentDate.date.toString()) // 달력 초기 데이터 (2022-04-01)
+        adapter = ScheduleAdapter()
+        binding.scheduleRvList.apply {
+            this.adapter = this@ScheduleFragment.adapter
+            this.layoutManager = LinearLayoutManager(requireActivity())
+            this.setHasFixedSize(true)
+            this.addItemDecoration(DividerItemDecoration(requireActivity(),1))
+        }
     }
 
     override fun initDataBinding() {
-
-
-
+        adapter.submitList(viewModel.testCode)
     }
 
     override fun initAfterBinding() {
@@ -33,6 +43,7 @@ class ScheduleFragment : BaseFragment<FragmentScheduleBinding, ScheduleViewModel
         // TODO :: 월 변경될 때마다 학사 일정 리스트도 변경하기
         binding.scheduleCalendar.setOnMonthChangedListener { _, date ->
             val currentDate = date.year.toString() + "." + date.month.toString() // 2022.4
+            adapter.submitList(viewModel.testCode2)
             log(currentDate)
         }
 
