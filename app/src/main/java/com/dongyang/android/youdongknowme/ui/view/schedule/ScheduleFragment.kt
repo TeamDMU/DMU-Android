5090254a8397b1d7c7de020a52e8dd45db642fd8
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-04-26 16:36:09 +0900

    feat : 학사 일정 캘린더뷰 기본 구성
    * MaterialCalendarView 기본 설정
    -> 연월만 화면에 나타나게 설정하였음.

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleFragment.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.FragmentScheduleBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseFragment
+import com.dongyang.android.youdongknowme.standard.util.log
 
 
 /* 학사 일정 화면 */
@@ -18,14 +19,22 @@ class ScheduleFragment : BaseFragment<FragmentScheduleBinding, ScheduleViewModel
     }
 
     override fun initStartView() {
-
+        log(binding.scheduleCalendar.currentDate.date.toString()) // 달력 초기 데이터 (2022-04-01)
     }
 
     override fun initDataBinding() {
 
+
+
     }
 
     override fun initAfterBinding() {
+
+        // TODO :: 월 변경될 때마다 학사 일정 리스트도 변경하기
+        binding.scheduleCalendar.setOnMonthChangedListener { _, date ->
+            val currentDate = date.year.toString() + "." + date.month.toString() // 2022.4
+            log(currentDate)
+        }
 
     }
 }
