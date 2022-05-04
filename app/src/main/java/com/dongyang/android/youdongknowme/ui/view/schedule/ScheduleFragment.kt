9b5f3ff6594b37f7ad7d2cd1f5c6bee2a144ff68
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-05-04 13:18:42 +0900

    feat : 학사 일정 연-월 표시 변경
    * 기존 월-연 순으로 보였던 구성을 연-월 표시로 변경하고 한글로 설정하였음.

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleFragment.kt
@@ -8,6 +8,7 @@ import com.dongyang.android.youdongknowme.databinding.FragmentScheduleBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseFragment
 import com.dongyang.android.youdongknowme.standard.util.log
 import com.dongyang.android.youdongknowme.ui.adapter.ScheduleAdapter
+import org.threeten.bp.LocalDate
 
 
 /* 학사 일정 화면 */
@@ -40,12 +41,29 @@ class ScheduleFragment : BaseFragment<FragmentScheduleBinding, ScheduleViewModel
 
     override fun initAfterBinding() {
 
-        // TODO :: 월 변경될 때마다 학사 일정 리스트도 변경하기
+        // TODO :: 월 변경될 때마다 일정 리스트도 변경하기
+        // TODO :: MinDate, MaxDate 설정 필요(리소스 감소)
         binding.scheduleCalendar.setOnMonthChangedListener { _, date ->
             val currentDate = date.year.toString() + "." + date.month.toString() // 2022.4
             adapter.submitList(viewModel.testCode2)
             log(currentDate)
         }
 
+        // 연/월 방식으로 타이틀 처리
+        binding.scheduleCalendar.setTitleFormatter { day ->
+            val inputText: LocalDate = day.date
+            val calendarHeaderElements = inputText.toString().split("-").toTypedArray()
+
+            val year = calendarHeaderElements[0]
+            val month = calendarHeaderElements[1]
+
+            val calendarHeaderBuilder = StringBuilder()
+            calendarHeaderBuilder.append(year)
+                .append(getString(R.string.calendar_year))
+                .append(" ")
+                .append(month)
+                .append(getString(R.string.calendar_month))
+            calendarHeaderBuilder.toString()
+        }
     }
 }
