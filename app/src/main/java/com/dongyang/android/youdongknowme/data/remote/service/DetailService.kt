b9e75e0a4ac7ede2b2a9144e87de98db4963140b
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-05-05 18:04:41 +0900

    refactor : 공지사항 상세 정보 Service 생성
    * 표현하는 화면이 다르니 Service 도 따로 뺴는 것이 옳다고 판단되어 분리하였음.

--- app/src/main/java/com/dongyang/android/youdongknowme/data/remote/service/DetailService.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/remote/service/DetailService.kt
@@ -1,15 +1,13 @@
 package com.dongyang.android.youdongknowme.data.remote.service
 
-import com.dongyang.android.youdongknowme.data.remote.entity.Notice
 import com.dongyang.android.youdongknowme.data.remote.entity.NoticeDetail
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-interface NoticeService {
-    @GET("/notice")
-    suspend fun getList(
-
-    ) : Response<List<Notice>>
+interface DetailService {
+    @GET("/notice/{number}")
+    suspend fun getNoticeDetail(
+        @Path("number") number : Int
+    ) : Response<NoticeDetail>
 }
-
--- app/src/main/java/com/dongyang/android/youdongknowme/data/repository/DetailRepository.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/repository/DetailRepository.kt
@@ -1,12 +1,12 @@
 package com.dongyang.android.youdongknowme.data.repository
 
 import com.dongyang.android.youdongknowme.data.remote.entity.NoticeDetail
-import com.dongyang.android.youdongknowme.data.remote.service.NoticeService
+import com.dongyang.android.youdongknowme.data.remote.service.DetailService
 import com.dongyang.android.youdongknowme.standard.network.RetrofitObject
 import retrofit2.Response
 
 class DetailRepository {
     suspend fun getNoticeDetail(num : Int) : Response<NoticeDetail> {
-        return RetrofitObject.getNetwork().create(NoticeService::class.java).getNoticeDetail(num)
+        return RetrofitObject.getNetwork().create(DetailService::class.java).getNoticeDetail(num)
     }
 }
