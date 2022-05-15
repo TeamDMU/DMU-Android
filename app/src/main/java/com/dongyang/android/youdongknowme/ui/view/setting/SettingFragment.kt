6c16211f4c88035a6bc48a772a8c838ceec52990
jiwon1454 <jiwon1454@naver.com>
2022-05-16 02:10:22 +0900

    Design: setting ui
    *setting ui 수정
    *app version 코드 추가

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/setting/SettingFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/setting/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.dongyang.android.youdongknowme.ui.view.setting
 
+import android.content.Context
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.FragmentSettingBinding
@@ -14,7 +15,20 @@ class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>()
     override val layoutResourceId: Int = R.layout.fragment_setting
     override val viewModel: SettingViewModel by viewModel()
 
+    //현재 app version 불러오기 (1.0.0)
+    private fun getAppVersion(context: Context):String? {
+        var versionName = ""
+        try {
+            val packageManager = context.packageManager.getPackageInfo(context.packageName,0)
+            versionName = packageManager.versionName
+        } catch (e:Exception){
+
+        }
+        return versionName
+    }
+
     override fun initStartView() {
+        binding!!.settingVersion.text =  "앱버전 " + getAppVersion(requireContext())
 
     }
 
