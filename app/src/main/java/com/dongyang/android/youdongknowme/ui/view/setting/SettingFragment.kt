da54c2332411a0e542b8ffe0d502d9f7702291bd
jiwon1454 <jiwon1454@naver.com>
2022-05-19 17:12:36 +0900

    Feat : Setting Function
    -Setting ui 수정
    -App Version
    -implement the e-mail function

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/setting/SettingFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/setting/SettingFragment.kt
@@ -1,10 +1,14 @@
 package com.dongyang.android.youdongknowme.ui.view.setting
 
 import android.content.Context
+import android.content.Intent
+import android.widget.Toast
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.FragmentSettingBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseFragment
+import com.dongyang.android.youdongknowme.ui.view.depart.DepartActivity
+import com.dongyang.android.youdongknowme.ui.view.main.MainActivity
 
 /* 설정 화면 */
 class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>() {
@@ -16,20 +20,16 @@ class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>()
     override val viewModel: SettingViewModel by viewModel()
 
     //현재 app version 불러오기 (1.0.0)
-    private fun getAppVersion(context: Context):String? {
-        var versionName = ""
-        try {
-            val packageManager = context.packageManager.getPackageInfo(context.packageName,0)
-            versionName = packageManager.versionName
-        } catch (e:Exception){
-
-        }
-        return versionName
+    private fun getAppVersion(): String {
+        val packageManager =
+            requireContext().packageManager.getPackageInfo(requireContext().packageName,0)
+            return packageManager.versionName
     }
 
-    override fun initStartView() {
-        binding!!.settingVersion.text =  "앱버전 " + getAppVersion(requireContext())
 
+
+    override fun initStartView() {
+        binding.settingVersion.text = " ${getAppVersion()}"
     }
 
     override fun initDataBinding() {
@@ -37,6 +37,19 @@ class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>()
     }
 
     override fun initAfterBinding() {
+        //이메일 연동 코드
+        binding.appHelp.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "plain/text"
+            val address = arrayOf("jiwon0705@m365.dongyang.ac.kr") //이메일 주소는 배열로 묶어주기
+            intent.putExtra(Intent.EXTRA_EMAIL, address)
+            startActivity(intent)
+        }
 
+        //학과 설정 눌렀을 때
+        binding.departmentChoice.setOnClickListener {
+            val intent = Intent(requireActivity(), DepartActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
