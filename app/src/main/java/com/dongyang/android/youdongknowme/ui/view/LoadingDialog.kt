2454444cf4287ea29663ae2e4df22b2c509e28ea
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-05-19 14:03:50 +0900

    feat : 로딩 다이얼로그 생성

---
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/LoadingDialog.kt
@@ -0,0 +1,17 @@
+package com.dongyang.android.youdongknowme.ui.view
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import com.dongyang.android.youdongknowme.R
+
+class LoadingDialog(context: Context) : Dialog(context) {
+
+    init {
+        setCanceledOnTouchOutside(false)
+
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setContentView(R.layout.dialog_loading)
+    }
+}
