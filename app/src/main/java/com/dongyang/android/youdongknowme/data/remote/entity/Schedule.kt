73620129077ab209147446ab940b76637bd177c2
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-05-21 19:22:59 +0900

    fix : 학사 일정 API 변경으로 인한 모델 변경

--- app/src/main/java/com/dongyang/android/youdongknowme/data/remote/entity/Schedule.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/remote/entity/Schedule.kt
@@ -1,6 +1,9 @@
 package com.dongyang.android.youdongknowme.data.remote.entity
 
-data class Schedule (
-    var date : String,
-    var contents : String
-    )
+
+data class Schedule(
+    var year: String,
+    var month: Int,
+    var date: String,
+    var content: String
+)
