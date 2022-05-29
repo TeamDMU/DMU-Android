0af3e98c0a1a40ae28feed35beddf20662240887
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-05-29 15:27:27 +0900

    feat : AWS 서버와 연결

--- app/src/main/java/com/dongyang/android/youdongknowme/standard/network/RetrofitObject.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/standard/network/RetrofitObject.kt
@@ -1,20 +1,32 @@
 package com.dongyang.android.youdongknowme.standard.network
 
+import com.dongyang.android.youdongknowme.BuildConfig
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitObject {
-    private const val TIME_OUT_COUNT : Long = 10
+    private const val TIME_OUT_COUNT: Long = 10
 
-    fun getNetwork() : Retrofit {
-        // val baseUrl = BuildConfig.SERVER
-         val baseUrl = "http://10.0.2.2:8000"
+    fun getNetwork(): Retrofit {
+        val baseUrl = "http://ec2-13-125-202-140.ap-northeast-2.compute.amazonaws.com:8000"
+
+        val baseInterceptor = Interceptor { chain ->
+            val request = chain.request().newBuilder()
+            val originalHttpUrl = chain.request().url
+
+            val url = originalHttpUrl.newBuilder().addQueryParameter("api_key", BuildConfig.API_KEY).build()
+            request.url(url)
+            chain.proceed(request.build())
+        }
 
         val client = OkHttpClient.Builder()
             .connectTimeout(TIME_OUT_COUNT, TimeUnit.SECONDS)
             .readTimeout(TIME_OUT_COUNT, TimeUnit.SECONDS)
+            .addInterceptor(baseInterceptor)
             .build()
 
         return Retrofit.Builder()
