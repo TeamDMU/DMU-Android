3dfef85bff55163dc72cda00ccd24ec06b442a53
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-06-09 22:39:45 +0900

    feat : 알람 설정 여부에 따라 파이어베이스 메시지 수신 여부를 결정하는 로직 추가

--- app/src/main/java/com/dongyang/android/youdongknowme/service/FCMService.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/service/FCMService.kt
@@ -13,6 +13,8 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.dongyang.android.youdongknowme.R
+import com.dongyang.android.youdongknowme.data.local.SharedPreference
+import com.dongyang.android.youdongknowme.standard.util.logd
 import com.dongyang.android.youdongknowme.standard.util.logw
 import com.dongyang.android.youdongknowme.ui.view.main.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -30,51 +32,33 @@ class FCMService : FirebaseMessagingService() {
         logw("New Token :: $token")
         super.onNewToken(token)
     }
-
-    // TODO :: 알림 설정을 하지 않은 유저에 대한 예외처리 필요
+    
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-//        val title = message.notification?.title ?: ""
-//        val content = message.notification?.body ?: ""
         val data = message.data
 
-        if (data["keyword"] != null) {
-            createNotificationChannel()
+        val major_code: String? = data["major_code"]
+        val notice_num: String? = data["num"]
+        val title: String? = data["title"]
+        val keyword: String? = data["keyword"]
 
-            // Foreground 에 있을 때 알림 처리
-            val splashIntent = Intent(this, MainActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            }
+        if (data.isNotEmpty()) {
+            val alarmArea = if(major_code == "1") "학교" else "학과"
 
-            val pendingIntent =
-                PendingIntent.getActivity(
-                    this, 0, splashIntent,
-                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-                )
-
-
-            val defaultSoundUri: Uri =
-                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-
-            val notificationBuilder = NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setColor(ContextCompat.getColor(this, R.color.main))
-                .setContentTitle("${data["keyword"]}(이)가 포함된 공지사항이 올라왔어요!")
-//                .setContentText(data["content"])
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-            with(NotificationManagerCompat.from(this)) {
-                val notificationId = SystemClock.uptimeMillis().toInt()
-                notify(notificationId, notificationBuilder.build())
+            if (major_code == "1") {
+                if (SharedPreference.getIsAccessSchoolAlarm()!!) {
+                    createNotificationChannel(keyword!!, alarmArea)
+                }
+            } else {
+                if (SharedPreference.getIsAccessDepartAlarm()!!) {
+                    createNotificationChannel(keyword!!, alarmArea)
+                }
             }
         }
     }
 
-    private fun createNotificationChannel() {
+    private fun createNotificationChannel(keyword: String, alarmArea: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val description = "FCM 메세지 알림"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
@@ -88,6 +72,34 @@ class FCMService : FirebaseMessagingService() {
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             notificationManager.createNotificationChannel(channel)
+        }
+
+        // Foreground 에 있을 때 알림 처리
+        val splashIntent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent =
+            PendingIntent.getActivity(
+                this, 0, splashIntent,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+            )
+
+        val defaultSoundUri: Uri =
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setColor(ContextCompat.getColor(this, R.color.main))
+            .setContentTitle("${keyword}(이)가 포함된 공지사항이 ${alarmArea}에 올라왔어요!")
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            val notificationId = SystemClock.uptimeMillis().toInt()
+            notify(notificationId, notificationBuilder.build())
         }
     }
 }
