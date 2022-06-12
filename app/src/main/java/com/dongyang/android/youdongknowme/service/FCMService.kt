9b2cd64253ad1cbfee161900cd163d2d86e0d96a
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-06-13 02:24:41 +0900

    fix : 알림이 왔을 때 알람 액티비티로 이동하도록 변경

--- app/src/main/java/com/dongyang/android/youdongknowme/service/FCMService.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/service/FCMService.kt
@@ -13,14 +13,12 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.data.local.SharedPreference
-import com.dongyang.android.youdongknowme.data.local.UserDatabase
 import com.dongyang.android.youdongknowme.data.local.dao.AlarmDao
 import com.dongyang.android.youdongknowme.data.local.entity.AlarmEntity
-import com.dongyang.android.youdongknowme.data.repository.FCMRepository
 import com.dongyang.android.youdongknowme.standard.util.logw
 import com.dongyang.android.youdongknowme.standard.util.mapDepartmentCodeToKorean
 import com.dongyang.android.youdongknowme.standard.util.mapKeywordEnglishToKorean
-import com.dongyang.android.youdongknowme.ui.view.keyword.KeywordActivity
+import com.dongyang.android.youdongknowme.ui.view.alarm.AlarmActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.CoroutineScope
@@ -64,7 +62,8 @@ class FCMService : FirebaseMessagingService() {
             val department = mapDepartmentCodeToKorean(major_code!!.toInt())
             val koreanKeyword = mapKeywordEnglishToKorean(keyword!!)
 
-            val alarmEntity = AlarmEntity(null, title!!, department, koreanKeyword, notice_num!!.toInt(), false)
+            val alarmEntity =
+                AlarmEntity(null, title!!, department, koreanKeyword, notice_num!!.toInt(), false)
             insertAlarmData(alarmEntity)
 
             if (department == "학교") {
@@ -98,7 +97,7 @@ class FCMService : FirebaseMessagingService() {
             notificationManager.createNotificationChannel(channel)
         }
 
-        val splashIntent = Intent(this, KeywordActivity::class.java).apply {
+        val splashIntent = Intent(this, AlarmActivity::class.java).apply {
             // flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
 
@@ -127,9 +126,9 @@ class FCMService : FirebaseMessagingService() {
     }
 
     private fun insertAlarmData(alarmEntity: AlarmEntity) {
-        val alarmDao : AlarmDao by inject()
+        val alarmDao: AlarmDao by inject()
 
-        if(alarmEntity.department == "학교") {
+        if (alarmEntity.department == "학교") {
             alarmEntity.department = "동양미래대학교"
         }
 
