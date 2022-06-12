d37676f7eba544487144965a2053bee2bf98edba
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-06-12 22:40:51 +0900

    feat : 알람 Repository, ViewModel 추가

---
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/repository/AlarmRepository.kt
@@ -0,0 +1,17 @@
+package com.dongyang.android.youdongknowme.data.repository
+
+import com.dongyang.android.youdongknowme.data.local.dao.AlarmDao
+import com.dongyang.android.youdongknowme.data.local.entity.AlarmEntity
+import kotlinx.coroutines.flow.Flow
+
+class AlarmRepository(
+    private val alarmDao: AlarmDao
+) {
+    fun getUserAlarms(): Flow<List<AlarmEntity>> {
+        return alarmDao.getAllAlarm()
+    }
+
+    suspend fun insertAlarm(alarmEntity: AlarmEntity) {
+        alarmDao.insertAlarm(alarmEntity)
+    }
+}
--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/alarm/AlarmViewModel.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/alarm/AlarmViewModel.kt
@@ -1,6 +1,32 @@
 package com.dongyang.android.youdongknowme.ui.view.alarm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.dongyang.android.youdongknowme.data.local.entity.AlarmEntity
+import com.dongyang.android.youdongknowme.data.repository.AlarmRepository
 import com.dongyang.android.youdongknowme.standard.base.BaseViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class AlarmViewModel : BaseViewModel() {
+class AlarmViewModel(private val alarmRepository: AlarmRepository) : BaseViewModel() {
+
+    private val _alarmList: MutableLiveData<List<AlarmEntity>> = MutableLiveData()
+    val alarmList: LiveData<List<AlarmEntity>> get() = _alarmList
+
+    fun getAlarms() {
+        viewModelScope.launch {
+            alarmRepository.getUserAlarms().collect { alarmList ->
+                _alarmList.postValue(alarmList)
+            }
+        }
+    }
+
+    fun insertAlarm() {
+        val testAlarm = AlarmEntity(null, "타이틀 테스트입니다.", "컴퓨터소프트웨어공학과", "시험", 10, false)
+        viewModelScope.launch {
+            alarmRepository.insertAlarm(testAlarm)
+        }
+    }
+
 }
