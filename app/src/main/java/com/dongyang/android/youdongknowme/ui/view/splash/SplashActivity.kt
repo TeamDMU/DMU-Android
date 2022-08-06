4bfd91278ebe42073edda68f7729b464f439c9e0
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-06 10:08:29 +0900

    feat : 버전이 12 이상일 경우 기존 스플래시 스크린이 보이지 않도록 설정

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/splash/SplashActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/splash/SplashActivity.kt
@@ -2,11 +2,12 @@ package com.dongyang.android.youdongknowme.ui.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.dongyang.android.youdongknowme.databinding.ActivitySplashBinding
-import com.dongyang.android.youdongknowme.standard.util.logd
 import com.dongyang.android.youdongknowme.ui.view.depart.DepartActivity
 import com.dongyang.android.youdongknowme.ui.view.main.MainActivity
 import kotlinx.coroutines.Job
@@ -27,10 +28,15 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val content: View = findViewById(android.R.id.content)
+            content.viewTreeObserver.addOnPreDrawListener { false }
+        }
+
         viewModel.checkFirstLaunch()
 
         intentJob = lifecycleScope.launch {
-            delay(2000L)
+            delay(1500L)
 
             if(viewModel.isFirstLaunch) {
                 val intent = Intent(this@SplashActivity, DepartActivity::class.java)
