7dae1e99612b0d28b83c771e6e238484ed89eb40
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-08 06:21:39 +0900

    feat : 식당 Repository 추가

---
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/repository/CafeteriaRepository.kt
@@ -0,0 +1,23 @@
+package com.dongyang.android.youdongknowme.data.repository
+
+import com.dongyang.android.youdongknowme.data.remote.entity.Cafeteria
+import com.dongyang.android.youdongknowme.data.remote.service.CafeteriaService
+import com.dongyang.android.youdongknowme.standard.network.ErrorResponseHandler
+import com.dongyang.android.youdongknowme.standard.network.NetworkResult
+import com.dongyang.android.youdongknowme.standard.network.RetrofitObject
+
+class CafeteriaRepository(
+    private val errorResponseHandler: ErrorResponseHandler
+) {
+
+    suspend fun fetchMenuList(code: Int, num: Int): NetworkResult<List<Cafeteria>> {
+
+        return try {
+            val response = RetrofitObject.getNetwork().create(CafeteriaService::class.java).getMenuList()
+            NetworkResult.Success(response)
+        } catch (exception: Exception) {
+            val error = errorResponseHandler.getError(exception)
+            NetworkResult.Error(error)
+        }
+    }
+}
