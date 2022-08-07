f746bb58a136d15910709e911ed443ffffda90bf
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-08 04:54:19 +0900

    refactor : 상세 화면의 API 결과물을 Response 타입으로 받지 않도록 수정

--- app/src/main/java/com/dongyang/android/youdongknowme/data/remote/service/DetailService.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/remote/service/DetailService.kt
@@ -1,7 +1,6 @@
 package com.dongyang.android.youdongknowme.data.remote.service
 
 import com.dongyang.android.youdongknowme.data.remote.entity.NoticeDetail
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -10,5 +9,5 @@ interface DetailService {
     suspend fun getNoticeDetail(
         @Path("code") code : Int,
         @Path("number") number : Int
-    ) : Response<NoticeDetail>
+    ) : NoticeDetail
 }
