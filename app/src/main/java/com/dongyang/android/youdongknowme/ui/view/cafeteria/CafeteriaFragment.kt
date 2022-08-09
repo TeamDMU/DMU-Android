5ae69b9b2fc2999b8e0c5b76642a6a29fb0f6ab2
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-10 03:28:59 +0900

    fix : 학식 액티비티의 코드 정리 및 바인딩 관련 오류 수정

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/cafeteria/CafeteriaFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/cafeteria/CafeteriaFragment.kt
@@ -2,22 +2,22 @@ package com.dongyang.android.youdongknowme.ui.view.cafeteria
 
 import android.content.Context
 import android.util.DisplayMetrics
-import android.util.Log
-import android.util.Size
 import android.view.View
 import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.FragmentCafeteriaBinding
+import com.dongyang.android.youdongknowme.databinding.ItemCalendarDayBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseFragment
 import com.dongyang.android.youdongknowme.ui.adapter.CafeteriaAdapter
 import com.dongyang.android.youdongknowme.ui.adapter.CafeteriaEmployeeAdapter
 import com.kizitonwose.calendarview.model.CalendarDay
 import com.kizitonwose.calendarview.ui.DayBinder
 import com.kizitonwose.calendarview.ui.ViewContainer
+import com.kizitonwose.calendarview.utils.Size
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.text.SimpleDateFormat
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
@@ -28,39 +28,56 @@ class CafeteriaFragment : BaseFragment<FragmentCafeteriaBinding, CafeteriaViewMo
     companion object {
         fun newInstance() = CafeteriaFragment()
     }
-    private var selectedDate : LocalDate?=null
-    //private var selectedDate = LocaleDate.now()
-
-    private val dateFormatter = SimpleDateFormat("dd")
-    private val dayFormatter = SimpleDateFormat("EEE")
-    private val monthFormatter = SimpleDateFormat("MMM")
-
-    //private val dateFormatter = DateTimeFormatter.ofPattern("dd")
-    //private val dayFormatter = DateTimeFormatter.ofPattern("EEE")
-    //private val monthFormatter = DateTimeFormatter.ofPattern("MMM")
 
     override val layoutResourceId: Int = R.layout.fragment_cafeteria
     override val viewModel: CafeteriaViewModel by viewModel()
 
+    private var selectedDate = LocalDate.now()
+
+    private val dateFormatter = DateTimeFormatter.ofPattern("dd")
+    private val dayFormatter = DateTimeFormatter.ofPattern("EEE")
+    private val monthFormatter = DateTimeFormatter.ofPattern("MMM")
+
     private lateinit var cafeteriaAdapter: CafeteriaAdapter
     private lateinit var cafeteriaEmployeeAdapter: CafeteriaEmployeeAdapter
 
-    private var menuList = arrayListOf("시금치된장국", "오이냉국","시금치된장국", "오이냉국","시금치된장국", "오이냉국","시금치된장국", "오이냉국")
-    private var employee_menuList = arrayListOf("짜장덮밥", "닭볶음탕","무생채","알감자조림","도시락김","치킨마요덮밥","매운닭갈비","간장")
+    private var menuList =
+        arrayListOf("시금치된장국", "오이냉국", "시금치된장국", "오이냉국", "시금치된장국", "오이냉국", "시금치된장국", "오이냉국")
+    private var employeeMenuList =
+        arrayListOf("짜장덮밥", "닭볶음탕", "무생채", "알감자조림", "도시락김", "치킨마요덮밥", "매운닭갈비", "간장")
 
     override fun initStartView() {
 
+        cafeteriaAdapter = CafeteriaAdapter()
+        cafeteriaEmployeeAdapter = CafeteriaEmployeeAdapter()
+
+        binding.stuMenuList.apply {
+            this.adapter = this@CafeteriaFragment.cafeteriaAdapter
+            this.layoutManager = LinearLayoutManager(requireActivity())
+            this.setHasFixedSize(true)
+            this.addItemDecoration(DividerItemDecoration(requireActivity(), 1))
+        }
+
+        binding.employeeMenuList.apply {
+            this.adapter = this@CafeteriaFragment.cafeteriaEmployeeAdapter
+            this.layoutManager = LinearLayoutManager(requireActivity())
+            this.setHasFixedSize(true)
+            this.addItemDecoration(DividerItemDecoration(requireActivity(), 1))
+        }
+
         val dm = DisplayMetrics()
         val wm = requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        // TODO : deprecated -> 수정 필요
         wm.defaultDisplay.getMetrics(dm)
-        binding.exSevenCalendar.apply{
+        binding.exSevenCalendar.apply {
             val dayWidth = dm.widthPixels / 5
             val dayHeight = (dayWidth * 1.25).toInt()
-            daySize = com.kizitonwose.calendarview.utils.Size(dayWidth,dayHeight)
+            daySize = Size(dayWidth, dayHeight)
         }
 
         class DayViewContainer(view: View) : ViewContainer(view) {
-            val bind = FragmentCafeteriaBinding.bind(view)
+            val bind = ItemCalendarDayBinding.bind(view)
             lateinit var day: CalendarDay
 
             init {
@@ -70,7 +87,6 @@ class CafeteriaFragment : BaseFragment<FragmentCafeteriaBinding, CafeteriaViewMo
                     if (firstDay == day) {
                         binding.exSevenCalendar.smoothScrollToDate(day.date)
                     } else if (lastDay == day) {
-
                         binding.exSevenCalendar.smoothScrollToDate(day.date.minusDays(4))
                     }
 
@@ -80,9 +96,6 @@ class CafeteriaFragment : BaseFragment<FragmentCafeteriaBinding, CafeteriaViewMo
                         binding.exSevenCalendar.notifyDateChanged(day.date)
                         oldDate?.let { binding.exSevenCalendar.notifyDateChanged(it) }
                     }
-
-                    Log.w("TAG :: ", selectedDate.toString())
-                    //binding.exSevenText.text = selectedDate.toString()
                 }
             }
 
@@ -92,8 +105,12 @@ class CafeteriaFragment : BaseFragment<FragmentCafeteriaBinding, CafeteriaViewMo
                 bind.DayText.text = dayFormatter.format(day.date)
                 bind.MonthText.text = monthFormatter.format(day.date)
 
-                bind.DateText.setTextColor(view.context.getColorCompat(if (day.date == selectedDate) R.color.black else R.color.white))
-                //bind.exSevenSelectedView.isVisible = day.date == selectedDate
+                bind.DateText.setTextColor(
+                    ContextCompat.getColor(
+                        view.context,
+                        if (day.date == selectedDate) R.color.main else R.color.black
+                    )
+                )
             }
         }
 
@@ -101,38 +118,24 @@ class CafeteriaFragment : BaseFragment<FragmentCafeteriaBinding, CafeteriaViewMo
             override fun create(view: View) = DayViewContainer(view)
             override fun bind(container: DayViewContainer, day: CalendarDay) = container.bind(day)
         }
-
-        cafeteriaAdapter = CafeteriaAdapter()
-        cafeteriaEmployeeAdapter = CafeteriaEmployeeAdapter()
-
-
-        binding.stuMenuList.apply {
-            this.adapter = this@CafeteriaFragment.cafeteriaAdapter
-            this.layoutManager = LinearLayoutManager(requireActivity())
-            this.setHasFixedSize(true)
-            this.addItemDecoration(DividerItemDecoration(requireActivity(), 1))
-        }
-
-        binding.employeeMenuList.apply{
-            this.adapter = this@CafeteriaFragment.cafeteriaEmployeeAdapter
-            this.layoutManager = LinearLayoutManager(requireActivity())
-            this.setHasFixedSize(true)
-            this.addItemDecoration(DividerItemDecoration(requireActivity(),1))
-        }
     }
 
     override fun initDataBinding() {
         cafeteriaAdapter.submitList(menuList)
-        cafeteriaEmployeeAdapter.submitList(employee_menuList)
+        cafeteriaEmployeeAdapter.submitList(employeeMenuList)
     }
 
     override fun initAfterBinding() {
-        //현재달
-        //val currentMonth = YearMonth.now()
-        //최소 달,최대 달 입력하는 부분
-        //binding.exSevenCalendar.setup(currentMonth.minusMonths(3), currentMonth.plusMonths(12), DayOfWeek.values().random())
-        //초기 날짜 세팅
-        //binding.exSevenCalendar.scrollToDate(LocaleDate)
+        // 현재달
+        val currentMonth = YearMonth.now()
+        // 최소 달,최대 달 입력하는 부분
+        binding.exSevenCalendar.setup(
+            currentMonth.minusMonths(3),
+            currentMonth.plusMonths(12),
+            DayOfWeek.values().random()
+        )
+        // 초기 날짜 세팅
+        binding.exSevenCalendar.scrollToDate(LocalDate.now())
     }
 }
 
