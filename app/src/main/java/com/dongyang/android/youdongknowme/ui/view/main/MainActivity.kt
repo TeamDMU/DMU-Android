5bc0343e657dca42486894e5df65f08ae91dbcc9
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-18 20:45:12 +0900

    refactor : 커스텀 네비게이터 적용
    * 이제 더 이상 프래그먼트의 상태를 잃지 않음

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
@@ -3,9 +3,11 @@ package com.dongyang.android.youdongknowme.ui.view.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.plusAssign
 import androidx.navigation.ui.setupWithNavController
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.ActivityMainBinding
+import com.dongyang.android.youdongknowme.ui.view.util.KeepStateNavigator
 
 
 /* 메인 액티비티 */
@@ -20,9 +22,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.main_nav_container) as NavHostFragment
         val navController = navHostFragment.navController
 
+        val navigator =
+            KeepStateNavigator(this, navHostFragment.childFragmentManager, binding.mainNavContainer.id)
+        navController.navigatorProvider += navigator
+        navController.setGraph(R.navigation.dmu_navigation)
         binding.mainNvBottom.setupWithNavController(navController)
     }
 }
