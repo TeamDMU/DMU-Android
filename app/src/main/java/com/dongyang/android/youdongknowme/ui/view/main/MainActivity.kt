cab060d92d269a0ec46d4ab934f46b580e23c261
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-21 23:19:47 +0900

    refactor : MainActivity 가 BaseActivity 를 상속받도록 수정

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.dongyang.android.youdongknowme.ui.view.main
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.plusAssign
 import androidx.navigation.ui.setupWithNavController
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.ActivityMainBinding
+import com.dongyang.android.youdongknowme.standard.base.BaseActivity
 import com.dongyang.android.youdongknowme.ui.view.util.KeepStateNavigator
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 /* 메인 액티비티 */
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
-    private lateinit var binding: ActivityMainBinding
+    override val layoutResourceId: Int = R.layout.activity_main
+    override val viewModel: MainViewModel by viewModel()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-
+    override fun initStartView() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_nav_container) as NavHostFragment
         val navController = navHostFragment.navController
@@ -31,4 +27,8 @@ class MainActivity : AppCompatActivity() {
         navController.setGraph(R.navigation.dmu_navigation)
         binding.mainNvBottom.setupWithNavController(navController)
     }
+
+    override fun initDataBinding() {}
+
+    override fun initAfterBinding() {}
 }
---
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.dongyang.android.youdongknowme.ui.view.main
+
+import com.dongyang.android.youdongknowme.standard.base.BaseViewModel
+
+class MainViewModel : BaseViewModel() {
+    // NOT IMPLEMENTS
+}
