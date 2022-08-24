093fa615bb3e5477a5ca86566cb83bed139ada49
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-24 21:15:35 +0900

    feat : 라이센스 액티비티의 베이스를 세팅

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/license/LicenseActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/license/LicenseActivity.kt
@@ -1,12 +1,23 @@
 package com.dongyang.android.youdongknowme.ui.view.license
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.dongyang.android.youdongknowme.R
+import com.dongyang.android.youdongknowme.databinding.ActivityLicenseBinding
+import com.dongyang.android.youdongknowme.standard.base.BaseActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LicenseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_license)
+class LicenseActivity : BaseActivity<ActivityLicenseBinding, LicenseViewModel>() {
+    override val layoutResourceId: Int = R.layout.activity_license
+    override val viewModel: LicenseViewModel by viewModel()
+
+    override fun initStartView() {
+
+    }
+
+    override fun initDataBinding() {
+
+    }
+
+    override fun initAfterBinding() {
+
     }
 }
