ee3fd32ae2c72d64df1fef9695f1399853413059
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-08-24 21:12:56 +0900

    feat : 라이센스 액티비티 최초 생성

---
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/license/LicenseActivity.kt
@@ -0,0 +1,12 @@
+package com.dongyang.android.youdongknowme.ui.view.license
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongyang.android.youdongknowme.R
+
+class LicenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+    }
+}
