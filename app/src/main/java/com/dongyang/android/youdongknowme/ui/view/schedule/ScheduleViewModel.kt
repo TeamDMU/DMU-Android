cf35a7501b4b23af69d48a3f4371079cd5a1024c
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-11-18 10:23:05 +0900

    feat : 학사일정 네트워크 에러 핸들러 추가

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleViewModel.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleViewModel.kt
@@ -22,15 +22,18 @@ class ScheduleViewModel(private val scheduleRepository: ScheduleRepository) : Ba
     val errorState: LiveData<Event<Int>> = _errorState
 
     private val _isLoading: MutableLiveData<Boolean> = MutableLiveData()
-    val isLoading: LiveData<Boolean> get() = _isLoading
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isError: MutableLiveData<Boolean> = MutableLiveData()
+    val isError: LiveData<Boolean> = _isError
 
     private val _scheduleList = MutableLiveData<List<Schedule>>()
-    val scheduleList: LiveData<List<Schedule>> get() = _scheduleList
+    val scheduleList: LiveData<List<Schedule>> = _scheduleList
 
     private val _pickYear = MutableLiveData<Int>()
 
     private val _pickMonth = MutableLiveData<Int>()
-    val pickMonth: LiveData<Int> get() = _pickMonth
+    val pickMonth: LiveData<Int> = _pickMonth
 
     fun setPickedDate(date: CalendarDay) {
         _pickYear.value = date.year
@@ -38,21 +41,22 @@ class ScheduleViewModel(private val scheduleRepository: ScheduleRepository) : Ba
     }
 
     fun getSchedules() {
-
         // 로컬에 저장한 데이터가 없으면 네트워크에서 데이터를 받아와 로컬에 저장 및 화면에 출력
         if (scheduleRepository.getLocalSchedules() == NO_SCHEDULE) {
             _isLoading.postValue(true)
-            viewModelScope.launch() {
+            viewModelScope.launch {
                 when (val result = scheduleRepository.fetchSchedules()) {
                     is NetworkResult.Success -> {
                         val scheduleList = result.data
                         // 선택한 연월 조건에 따라 리스트 출력
                         _scheduleList.postValue(scheduleList.filter { it.month == pickMonth.value && it.year == _pickYear.value.toString() })
                         scheduleRepository.setLocalSchedules(Gson().toJson(scheduleList))
+                        _isError.postValue(false)
                         _isLoading.postValue(false)
                     }
                     is NetworkResult.Error -> {
                         handleError(result, _errorState)
+                        _isError.postValue(true)
                         _isLoading.postValue(false)
                     }
                 }
@@ -61,11 +65,15 @@ class ScheduleViewModel(private val scheduleRepository: ScheduleRepository) : Ba
             // 저장한 데이터가 있으면 로컬에서 곧바로 데이터를 받아와 화면에 출력
             val localSchedules = scheduleRepository.getLocalSchedules()
             val scheduleList =
-                Gson().fromJson<List<Schedule>>(localSchedules, object : TypeToken<List<Schedule>>() {}.type).filter {
+                Gson().fromJson<List<Schedule>>(
+                    localSchedules,
+                    object : TypeToken<List<Schedule>>() {}.type
+                ).filter {
                     // 선택한 연월 조건에 따라 리스트 출력
                     it.month == pickMonth.value && it.year == _pickYear.value.toString()
                 }
 
+            _isError.postValue(false)
             _scheduleList.postValue(scheduleList)
         }
     }
