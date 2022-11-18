fda29291cb8734b85d48ce43de79418a14f5061d
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-11-18 10:50:21 +0900

    feat : 오픈소스 목록에 flexbox-layout 추가

--- app/src/main/java/com/dongyang/android/youdongknowme/data/local/entity/OpenSourceEntity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/local/entity/OpenSourceEntity.kt
@@ -10,47 +10,52 @@ data class OpenSourceEntity(
             OpenSourceEntity(
                 "Firebase",
                 "https://firebase.google.com/terms",
-                "Apache"
+                "Apache License"
             ),
             OpenSourceEntity(
                 "glide",
                 "https://github.com/bumptech/glide/blob/master/LICENSE",
-                "Apache"
+                "Apache License"
             ),
             OpenSourceEntity(
                 "okhttp",
                 "https://github.com/square/okhttp/blob/master/LICENSE.txt",
-                "Apache"
+                "Apache License"
             ),
             OpenSourceEntity(
                 "retrofit",
                 "https://github.com/square/retrofit/blob/master/LICENSE.txt",
-                "Apache"
+                "Apache License"
             ),
             OpenSourceEntity(
                 "gson",
                 "https://github.com/google/gson/blob/master/LICENSE",
-                "Apache"
+                "Apache License"
             ),
             OpenSourceEntity(
                 "AndroidViewAnimations",
                 "https://github.com/daimajia/AndroidViewAnimations/blob/master/License",
-                "MIT"
+                "MIT License"
             ),
             OpenSourceEntity(
                 "lottie",
                 "https://github.com/airbnb/lottie-android/blob/master/LICENSE",
-                "Apache"
+                "Apache License"
             ),
             OpenSourceEntity(
                 "MaterialCalendarView",
                 "https://github.com/prolificinteractive/material-calendarview/blob/master/LICENSE",
-                "MIT"
+                "MIT License"
             ),
             OpenSourceEntity(
                 "CalendarView",
                 "https://github.com/kizitonwose/CalendarView/blob/master/LICENSE.md",
-                "MIT"
+                "MIT License"
+            ),
+            OpenSourceEntity(
+                "flexbox-layout",
+                "https://github.com/google/flexbox-layout/blob/main/LICENSE",
+                "Apache License"
             ),
         )
     }
