a53ce01fbd73ff5050e05a6e0255f5c05f3259ab
Son Myeongji <sonmyungj1zz@gmail.com>
2024-02-06 00:15:31 +0900

    feat : 검색 결과 지우는 버튼 가시성 처리 및 로직 구현

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/search/SearchFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/search/SearchFragment.kt
@@ -1,5 +1,8 @@
 package com.dongyang.android.youdongknowme.ui.view.search
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.FragmentSearchBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseFragment
@@ -15,6 +18,34 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
     }
 
     override fun initDataBinding() {
+        setTextClearButtonVisibility()
+        setTextClearButtonClickListener()
+    }
+
+    private fun setTextClearButtonVisibility() {
+        binding.etSearchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.validateSearchContent()
+            }
+        })
+
+        viewModel.searchValidation.observe(viewLifecycleOwner) { isValid ->
+            binding.ivSearchCancel.visibility = if (isValid) View.VISIBLE else View.GONE
+        }
+    }
+
+    private fun setTextClearButtonClickListener() {
+        with(binding) {
+            ivSearchCancel.setOnClickListener {
+                etSearchBar.text.clear()
+            }
+        }
     }
 
     override fun initAfterBinding() {
--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/search/SearchViewModel.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/search/SearchViewModel.kt
@@ -1,6 +1,17 @@
 package com.dongyang.android.youdongknowme.ui.view.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.dongyang.android.youdongknowme.standard.base.BaseViewModel
 
 class SearchViewModel : BaseViewModel() {
+    private val _searchContent: MutableLiveData<String> = MutableLiveData()
+    val searchContent: MutableLiveData<String> = _searchContent
+
+    private val _searchValidation: MutableLiveData<Boolean> = MutableLiveData()
+    val searchValidation: LiveData<Boolean> = _searchValidation
+
+    fun validateSearchContent() {
+        _searchValidation.value = !_searchContent.value.isNullOrBlank()
+    }
 }
