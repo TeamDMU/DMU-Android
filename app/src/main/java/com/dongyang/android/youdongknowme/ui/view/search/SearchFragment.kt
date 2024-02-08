2cc42c24f689faf6f00b9911fe715000b46dfed3
Son Myeongji <sonmyungj1zz@gmail.com>
2024-02-08 15:49:44 +0900

    refactor : 불필요한 코드 제거

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/search/SearchFragment.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/search/SearchFragment.kt
@@ -1,11 +1,9 @@
 package com.dongyang.android.youdongknowme.ui.view.search
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.FragmentSearchBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseFragment
@@ -35,11 +33,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
     private fun showKeyboardOnEditTextFocus() {
         binding.etSearchBar.requestFocus()
-
-        binding.etSearchBar.post {
-            binding.etSearchBar.setSelection(binding.etSearchBar.text.length)
-            requireContext().showKeyboard(binding.etSearchBar)
-        }
+        requireContext().showKeyboard(binding.etSearchBar)
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -52,8 +46,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
 
     private fun setTextClearButtonVisibility() {
         binding.etSearchBar.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) =
-                Unit
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
 
