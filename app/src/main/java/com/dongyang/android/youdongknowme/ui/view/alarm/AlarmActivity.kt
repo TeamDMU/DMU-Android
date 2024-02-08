b419d8a5074ca3647c00768f028c975a320a5335
Son Myeongji <sonmyungj1zz@gmail.com>
2024-02-08 17:16:55 +0900

    build : 알람 어댑터 제거

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/alarm/AlarmActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/alarm/AlarmActivity.kt
@@ -7,7 +7,6 @@ import com.dongyang.android.youdongknowme.data.local.entity.AlarmEntity
 import com.dongyang.android.youdongknowme.databinding.ActivityAlarmBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseActivity
 import com.dongyang.android.youdongknowme.standard.util.mapDepartmentKoreanToCode
-import com.dongyang.android.youdongknowme.ui.adapter.AlarmAdapter
 import com.dongyang.android.youdongknowme.ui.view.detail.DetailActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -15,22 +14,11 @@ class AlarmActivity : BaseActivity<ActivityAlarmBinding, AlarmViewModel>(), Alar
     override val layoutResourceId: Int = R.layout.activity_alarm
     override val viewModel: AlarmViewModel by viewModel()
 
-    private lateinit var adapter : AlarmAdapter
-
     override fun initStartView() {
-        adapter = AlarmAdapter().apply { setItemClickListener(this@AlarmActivity) }
         binding.vm = viewModel
-        binding.alarmRcv.apply {
-            this.adapter = this@AlarmActivity.adapter
-            this.layoutManager = LinearLayoutManager(this@AlarmActivity)
-            this.setHasFixedSize(true)
-        }
     }
 
     override fun initDataBinding() {
-        viewModel.alarmList.observe(this) {
-            adapter.submitList(it)
-        }
     }
 
     override fun initAfterBinding() {
