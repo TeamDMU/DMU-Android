e8c3f6b417c8112c4e75555ccdc0ee0929ab6f59
조희우 <happyhw0589@gmail.com>
2024-02-12 20:16:54 +0900

    refactor : 온보딩 알림 설정 viewModel 연결

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/settingAlarm/SettingAlarmActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/settingAlarm/SettingAlarmActivity.kt
@@ -2,11 +2,25 @@ package com.dongyang.android.youdongknowme.ui.view.settingAlarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.plusAssign
+import androidx.navigation.ui.setupWithNavController
 import com.dongyang.android.youdongknowme.R
+import com.dongyang.android.youdongknowme.databinding.ActivitySettingAlarmBinding
+import com.dongyang.android.youdongknowme.standard.base.BaseActivity
+import com.dongyang.android.youdongknowme.ui.view.keyword.KeywordViewModel
+import com.dongyang.android.youdongknowme.ui.view.util.KeepStateNavigator
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SettingAlarmActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting_alarm)
+class SettingAlarmActivity : BaseActivity<ActivitySettingAlarmBinding, SettingAlarmViewModel>() {
+
+    override val layoutResourceId: Int = R.layout.activity_setting_alarm
+    override val viewModel: SettingAlarmViewModel by viewModel()
+
+    override fun initStartView() {
     }
+
+    override fun initDataBinding() {}
+
+    override fun initAfterBinding() {}
 }
