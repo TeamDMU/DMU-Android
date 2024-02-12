359f14cb9cf2f9197c4bd74a2f089fc4c7bf6df8
조희우 <happyhw0589@gmail.com>
2024-02-12 21:22:18 +0900

    design : 온보딩 알림 설정 - 알림설정 박스 색상 변경

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/settingAlarm/SettingAlarmViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.dongyang.android.youdongknowme.ui.view.settingAlarm
-
-import com.dongyang.android.youdongknowme.standard.base.BaseViewModel
-
-class SettingAlarmViewModel : BaseViewModel() {
-}
