cd1e685a330621b5e33caad3a3542dabe6e6d31e
조희우 <happyhw0589@gmail.com>
2024-02-12 21:27:57 +0900

    style : 온보딩 알림 - 사용하지 않는 라이브러리 정리

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/settingAlarm/SettingAlarmActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/settingAlarm/SettingAlarmActivity.kt
@@ -2,22 +2,13 @@ package com.dongyang.android.youdongknowme.ui.view.settingAlarm
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.plusAssign
-import androidx.navigation.ui.setupWithNavController
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.ActivitySettingAlarmBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseActivity
-import com.dongyang.android.youdongknowme.ui.view.keyword.KeywordViewModel
 import com.dongyang.android.youdongknowme.ui.view.main.MainActivity
 import com.dongyang.android.youdongknowme.ui.view.setting.SettingViewModel
-import com.dongyang.android.youdongknowme.ui.view.util.KeepStateNavigator
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.security.AccessController.getContext
 
 class SettingAlarmActivity : BaseActivity<ActivitySettingAlarmBinding, SettingViewModel>() {
 
