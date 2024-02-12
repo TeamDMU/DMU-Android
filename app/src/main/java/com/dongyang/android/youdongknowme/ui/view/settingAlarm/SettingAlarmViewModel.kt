dd4c8ff1d40f9cd43aface1a78931df61f2746cc
조희우 <happyhw0589@gmail.com>
2024-02-12 20:00:31 +0900

    chore : SettingAlarmViewModel 생성

---
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/settingAlarm/SettingAlarmViewModel.kt
@@ -0,0 +1,6 @@
+package com.dongyang.android.youdongknowme.ui.view.settingAlarm
+
+import com.dongyang.android.youdongknowme.standard.base.BaseViewModel
+
+class SettingAlarmViewModel : BaseViewModel() {
+}
