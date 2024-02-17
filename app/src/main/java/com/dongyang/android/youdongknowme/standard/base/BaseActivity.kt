36551d72fdae2e47d2e7115fab127688e2e4a8c6
조희우 <happyhw0589@gmail.com>
2024-02-18 01:20:34 +0900

    chore : 텍스트 부분 색상 지정 함수명 변경

--- app/src/main/java/com/dongyang/android/youdongknowme/standard/base/BaseActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/standard/base/BaseActivity.kt
@@ -31,7 +31,7 @@ abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatA
         initDataBinding()
         initAfterBinding()
     }
-    protected fun setSpan(spanTextView: TextView, startIdx: Int, endIdx: Int){
+    protected fun setSpanText(spanTextView: TextView, startIdx: Int, endIdx: Int){
         SpannableStringBuilder(spanTextView.text).apply {
             setSpan(
                 ForegroundColorSpan(getColor(com.dongyang.android.youdongknowme.R.color.main)),
