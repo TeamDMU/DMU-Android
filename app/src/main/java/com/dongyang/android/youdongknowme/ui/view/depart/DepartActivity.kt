036c21507e67bb61e88a9a6caa02410a1c6b80cb
조희우 <happyhw0589@gmail.com>
2024-02-20 22:34:54 +0900

    refactor : 학과 선택 BaseActivity 기반 코드로 수정

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/depart/DepartActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/depart/DepartActivity.kt
@@ -1,32 +1,30 @@
 package com.dongyang.android.youdongknowme.ui.view.depart
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.ActivityDepartBinding
-import com.dongyang.android.youdongknowme.function.setSpanText
+import com.dongyang.android.youdongknowme.standard.base.BaseActivity
 import com.dongyang.android.youdongknowme.ui.adapter.DepartAdapter
 import com.dongyang.android.youdongknowme.ui.view.main.MainActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class DepartActivity : AppCompatActivity(), DepartClickListener {
+class DepartActivity : BaseActivity<ActivityDepartBinding, DepartViewModel>(), DepartClickListener {
 
-    private lateinit var adapter: DepartAdapter
-    private lateinit var binding: ActivityDepartBinding
-    private val viewModel: DepartViewModel by viewModel()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityDepartBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override val layoutResourceId: Int = R.layout.activity_depart
+    override val viewModel: DepartViewModel by viewModel()
+    lateinit var adapter: DepartAdapter
+    private lateinit var items: ArrayList<String>
 
-        // 학과 리스트
-        val items =
+    override fun initStartView() {
+        binding.vm = viewModel
+
+        items =
             resources.getStringArray(R.array.dmu_department_list).toCollection(ArrayList<String>())
         items.sort()
 
+        // 학과 리스트
         adapter = DepartAdapter().apply {
             submitList(items)
             setItemClickListener(this@DepartActivity)
@@ -38,7 +36,11 @@ class DepartActivity : AppCompatActivity(), DepartClickListener {
             this.setHasFixedSize(true)
         }
 
-        // 선택 포지션을 실시간 옵저빙
+    }
+
+    override fun initDataBinding() = Unit
+
+    override fun initAfterBinding() {
         viewModel.selectDepartPosition.observe(this) {
             adapter.submitPosition(it)
 
@@ -46,7 +48,7 @@ class DepartActivity : AppCompatActivity(), DepartClickListener {
             if (it != -1) getDepart(items)
         }
 
-        binding.toolbarDepart.toolbarExit.setOnClickListener { finish() }
+        binding.toolbarDepart.btnToolbarExit.setOnClickListener { finish() }
     }
 
     // 컨테이너 클릭 시 선택한 학과의 포지션 저장
@@ -56,7 +58,7 @@ class DepartActivity : AppCompatActivity(), DepartClickListener {
 
     // 확인 버튼을 누르면 내부 DB에 학과를 담고 메인 액티비티로 이동
     private fun getDepart(items: ArrayList<String>) {
-        return binding.btnDepartNext.setOnClickListener {
+        return binding.btnDepartComplete.setOnClickListener {
             viewModel.setDepartment(items[viewModel.selectDepartPosition.value ?: 0])
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
