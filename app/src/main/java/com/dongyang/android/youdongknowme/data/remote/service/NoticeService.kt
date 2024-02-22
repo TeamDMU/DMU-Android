bdaace77d827c79a09f5146519cc407ba9b0c3da
Son Myeongji <sonmyungj1zz@gmail.com>
2024-02-22 14:52:27 +0900

    feat : 학과 공지 api service 추가

--- app/src/main/java/com/dongyang/android/youdongknowme/data/remote/service/NoticeService.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/data/remote/service/NoticeService.kt
@@ -2,11 +2,19 @@ package com.dongyang.android.youdongknowme.data.remote.service
 
 import com.dongyang.android.youdongknowme.data.remote.entity.Notice
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NoticeService {
     @GET("api/v1/dmu/universityNotice")
     suspend fun getUniversityNotice(
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): List<Notice>
+
+    @GET("api/v1/dmu/departmentNotice/{department}")
+    suspend fun getDepartmentNotice(
+        @Path("department") department: String,
         @Query("page") page: Int,
         @Query("size") size: Int
     ): List<Notice>
