295fc3a121c2bd9a423ca263ce7befea28777613
조희우 <happyhw0589@gmail.com>
2024-02-22 20:06:08 +0900

    refactor : 미사용 라이브러리 제거

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/cafeteria/CafeteriaViewModel.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/cafeteria/CafeteriaViewModel.kt
@@ -1,6 +1,5 @@
 package com.dongyang.android.youdongknowme.ui.view.cafeteria
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
