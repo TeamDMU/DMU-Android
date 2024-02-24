833bd1eb234098f5af01a2eb63ecd7406352af1a
Son Myeongji <sonmyungj1zz@gmail.com>
2024-02-24 23:26:24 +0900

    feat : 웹 뷰의 파일 다운로드

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/detail/DetailActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/detail/DetailActivity.kt
@@ -1,8 +1,12 @@
 package com.dongyang.android.youdongknowme.ui.view.detail
 
 import android.annotation.SuppressLint
+import android.app.DownloadManager
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.webkit.URLUtil
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import com.dongyang.android.youdongknowme.R
@@ -24,6 +28,31 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>() {
 
             webViewClient = WebViewClient()
             webChromeClient = WebChromeClient()
+
+            setDownloadListener { url, user, contentDisposition, mimetype, contentLength ->
+                val request = DownloadManager.Request(Uri.parse(url)).apply {
+                    // 다운로드에 대한 제목, 설명 설정
+                    setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype))
+                    setDescription("Downloading file...")
+
+                    // 알림 표시줄에 다운로드 진행 상태 표시
+                    setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+
+                    // 파일 다운로드 경로 설정
+                    setDestinationInExternalPublicDir(
+                        Environment.DIRECTORY_DOWNLOADS,
+                        URLUtil.guessFileName(url, contentDisposition, mimetype)
+                    )
+
+                    // 모바일 네트워크 및 와이파이에서 다운로드 허용 설정
+                    setAllowedOverMetered(true)
+                    setAllowedOverRoaming(true)
+                }
+
+                // DownloadManager를 사용하여 다운로드 요청
+                val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                downloadManager.enqueue(request)
+            }
 
             loadUrl(url.toString())
         }
