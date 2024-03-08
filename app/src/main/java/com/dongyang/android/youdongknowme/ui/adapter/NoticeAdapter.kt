549054790ff57d4d164b34700af47f5dd29f250c
Son Myeongji <sonmyungj1zz@gmail.com>
2024-03-08 23:00:00 +0900

    style : 코트 포맷팅

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/adapter/NoticeAdapter.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/adapter/NoticeAdapter.kt
@@ -1,7 +1,6 @@
 package com.dongyang.android.youdongknowme.ui.adapter
 
 import android.annotation.SuppressLint
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
