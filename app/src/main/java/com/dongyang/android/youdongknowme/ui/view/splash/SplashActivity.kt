1d0da5d43bc0febe7745832b0f2fab538e2333f0
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2024-03-18 06:02:10 +0900

    refactor : 스플래시 화면 처음 시작시 권한 요청

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/splash/SplashActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/splash/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.dongyang.android.youdongknowme.ui.view.splash
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.ActivitySplashBinding
@@ -16,13 +20,12 @@ import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity () :
-    BaseActivity<ActivitySplashBinding, SplashViewModel>()  {
+class SplashActivity() : BaseActivity<ActivitySplashBinding, SplashViewModel>() {
 
     override val layoutResourceId: Int = R.layout.activity_splash
     override val viewModel: SplashViewModel by viewModel()
 
-     private var intentJob: Job? = null
+    private var intentJob: Job? = null
 
     override fun initStartView() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -39,10 +42,25 @@ class SplashActivity () :
         intentJob = lifecycleScope.launch {
             delay(SPLASH_TIME_MILLIS)
 
-            if(viewModel.isFirstLaunch) {
+            if (viewModel.isFirstLaunch.value == true) {
                 val intent = Intent(this@SplashActivity, OnboardingDepartActivity::class.java)
-                startActivity(intent)
-                finish()
+
+                if (PackageManager.PERMISSION_DENIED == ContextCompat.checkSelfPermission(
+                        this@SplashActivity,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    )
+                ) {
+                    // 권한 설정이 안되어 있는 경우
+                    ActivityCompat.requestPermissions(
+                        this@SplashActivity,
+                        arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                        100
+                    )
+                } else {
+                    // 권한 설정이 이미 되어 있는 경우
+                    startActivity(intent)
+                    finish()
+                }
             } else {
                 val intent = Intent(this@SplashActivity, MainActivity::class.java)
                 startActivity(intent)
@@ -50,6 +68,25 @@ class SplashActivity () :
             }
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100) {
+            // 권한 설정 결과 처리
+            val intent = if (viewModel.isFirstLaunch.value == true) {
+                Intent(this@SplashActivity, OnboardingDepartActivity::class.java)
+            } else {
+                Intent(this@SplashActivity, MainActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        }
+    }
+
 
     override fun onBackPressed() {
         intentJob?.cancel()
