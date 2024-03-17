94f289b135ead96e391ab108705ed9ec390002d9
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2024-03-18 06:03:21 +0900

    refactor : 권한 설정 확장함수 제거
    - 스플래시에서 권한 설정 후 화면이 넘어오면서 스위치 조정에 이상이 있음. 수정 필요.

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/permission/OnboardingPermissionActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/permission/OnboardingPermissionActivity.kt
@@ -1,17 +1,19 @@
 package com.dongyang.android.youdongknowme.ui.view.permission
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.ColorStateList
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.dongyang.android.youdongknowme.R
 import com.dongyang.android.youdongknowme.databinding.ActivityOnboardingPermissionBinding
 import com.dongyang.android.youdongknowme.standard.base.BaseActivity
 import com.dongyang.android.youdongknowme.ui.view.main.MainActivity
 import com.dongyang.android.youdongknowme.ui.view.setting.SettingViewModel
-import com.dongyang.android.youdongknowme.ui.view.util.RequestPermission
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class OnboardingPermissionActivity(private val requestPermission: RequestPermission) :
+class OnboardingPermissionActivity() :
     BaseActivity<ActivityOnboardingPermissionBinding, SettingViewModel>() {
 
     override val layoutResourceId: Int = R.layout.activity_onboarding_permission
@@ -20,6 +22,7 @@ class OnboardingPermissionActivity(private val requestPermission: RequestPermiss
     override fun initStartView() {
         viewModel.checkAccessAlarm()
         viewModel.getUserDepartment()
+        setPermissionSwitch(false)
 
         setSpanText(this, binding.tvPermissionTitleMain, startIdx = 0, endIdx = 9)
     }
@@ -33,18 +36,17 @@ class OnboardingPermissionActivity(private val requestPermission: RequestPermiss
             finish()
         }
 
-        setPermissionSwitch(false)
-
         binding.switchPermission.setOnCheckedChangeListener { compoundButton, _ ->
-            val isPermissionGranted =
-                requestPermission.isPermissionGranted()
-
             if (compoundButton.isChecked) {
-                if (isPermissionGranted) {
+                if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.POST_NOTIFICATIONS
+                    )
+                ) {
                     setPermissionSwitch(true)
                 } else {
-                    requestPermission.requestPermission(0)
-                    setPermissionSwitch(false)
+                    ActivityCompat.requestPermissions(
+                        this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 100
+                    )
                 }
             } else {
                 setPermissionSwitch(false)
