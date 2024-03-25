018fa04b48cd1818c319b18fba9881b50fdcb861
Son Myeongji <sonmyungj1zz@gmail.com>
2024-03-25 20:13:56 +0900

    refactor : 사용하지 않는 바인딩 어댑터 제거

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/adapter/BindingAdapter.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/adapter/BindingAdapter.kt
@@ -1,13 +1,7 @@
 package com.dongyang.android.youdongknowme.ui.adapter
 
 import android.view.View
-import androidx.appcompat.widget.SwitchCompat
 import androidx.databinding.BindingAdapter
-
-@BindingAdapter("bind_is_checked")
-fun bindIsChecked(view: SwitchCompat, condition: Boolean) {
-    view.isChecked = condition
-}
 
 @BindingAdapter("bind_is_loading", "bind_is_error")
 fun bindShowScreen(view: View, isLoading: Boolean, isError: Boolean) {
