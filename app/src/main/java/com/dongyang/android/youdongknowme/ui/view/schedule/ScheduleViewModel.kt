5ff50942e51b1a878e58e5f692bacb60a6bd36f8
조희우 <huiwoo.jo@gmail.com>
2025-01-16 11:00:57 +0900

    refactor: 로컬 저장 방식 변경

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleViewModel.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/schedule/ScheduleViewModel.kt
@@ -3,15 +3,12 @@ package com.dongyang.android.youdongknowme.ui.view.schedule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.dongyang.android.youdongknowme.data.local.SharedPreference.NO_SCHEDULE
 import com.dongyang.android.youdongknowme.data.remote.entity.Schedule
 import com.dongyang.android.youdongknowme.data.remote.entity.ScheduleEntry
 import com.dongyang.android.youdongknowme.data.repository.ScheduleRepository
 import com.dongyang.android.youdongknowme.standard.base.BaseViewModel
 import com.dongyang.android.youdongknowme.standard.network.NetworkResult
 import com.dongyang.android.youdongknowme.ui.view.util.Event
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import kotlinx.coroutines.launch
 
@@ -42,41 +39,25 @@ class ScheduleViewModel(private val scheduleRepository: ScheduleRepository) : Ba
     }
 
     fun getSchedules() {
-        // 로컬에 저장한 데이터가 없으면 네트워크에서 데이터를 받아와 로컬에 저장 및 화면에 출력
-        if (scheduleRepository.getLocalSchedules() == NO_SCHEDULE) {
-            _isLoading.postValue(true)
-            viewModelScope.launch {
-                when (val result = scheduleRepository.fetchSchedules()) {
-                    is NetworkResult.Success -> {
-                        val scheduleList = result.data
+        _isLoading.postValue(true)
+        viewModelScope.launch {
+            when (val result = scheduleRepository.fetchSchedules()) {
+                is NetworkResult.Success -> {
+                    val scheduleList = result.data
 
-                        // 선택한 연월 조건에 따라 리스트 출력
+                    // 선택한 연월 조건에 따라 리스트 출력
+                    _scheduleList.postValue(getSchedulesForPickDate(scheduleList))
 
-                        _scheduleList.postValue(getSchedulesForPickDate(scheduleList))
+                    _isError.postValue(false)
+                    _isLoading.postValue(false)
+                }
 
-                        scheduleRepository.setLocalSchedules(Gson().toJson(scheduleList))
-                        _isError.postValue(false)
-                        _isLoading.postValue(false)
-                    }
-
-                    is NetworkResult.Error -> {
-                        handleError(result, _errorState)
-                        _isError.postValue(true)
-                        _isLoading.postValue(false)
-                    }
+                is NetworkResult.Error -> {
+                    handleError(result, _errorState)
+                    _isError.postValue(true)
+                    _isLoading.postValue(false)
                 }
             }
-        } else {
-            // 저장한 데이터가 있으면 로컬에서 곧바로 데이터를 받아와 화면에 출력
-            val localSchedules = scheduleRepository.getLocalSchedules()
-            val scheduleList = getSchedulesForPickDate(
-                Gson().fromJson(
-                    localSchedules,
-                    object : TypeToken<List<Schedule>>() {}.type
-                )
-            )
-            _isError.postValue(false)
-            _scheduleList.postValue(scheduleList)
         }
     }
 
