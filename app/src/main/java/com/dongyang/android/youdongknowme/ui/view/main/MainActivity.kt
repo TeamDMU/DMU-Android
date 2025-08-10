20d6ba6d9c527a93c0ecd57fdda1b31da597fe33
m6z1 <sonmyungj1zz@gmail.com>
2025-08-10 18:36:25 +0900

    feat: edgeToEdge 대응

--- app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/dongyang/android/youdongknowme/ui/view/main/MainActivity.kt
@@ -3,7 +3,12 @@ package com.dongyang.android.youdongknowme.ui.view.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -24,9 +29,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
         setContentView(binding.root)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val inset = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updatePadding(
+                top = inset.top,
+                bottom = inset.bottom,
+                left = inset.left,
+                right = inset.right,
+            )
+            insets
+        }
 
         initDataBinding()
         initBottomNavigation()
